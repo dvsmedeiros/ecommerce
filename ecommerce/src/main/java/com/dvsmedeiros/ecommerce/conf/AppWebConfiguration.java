dbfc5604426d19ff2ac5062ee350456497bc2ed7
damedeiros <davisson.medeiros@compsisnet.com.br>
2017-02-27 00:01:23 -0300

    add configuração para as views

--- ecommerce/src/main/java/com/dvsmedeiros/ecommerce/conf/AppWebConfiguration.java
+++ ecommerce/src/main/java/com/dvsmedeiros/ecommerce/conf/AppWebConfiguration.java
@@ -5,19 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.dvsmedeiros")
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
     
-    @Override
+	@Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/public/");
+        viewResolver.setSuffix(".html");
+        registry.viewResolver(viewResolver);
+    }
+    
+	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         
-        registry.addResourceHandler("/resources/public").addResourceLocations("/public/");
+        registry.addResourceHandler("/public/**").addResourceLocations("/public/");
     }
-     
+	
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
