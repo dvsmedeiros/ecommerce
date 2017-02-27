59b93195484904f3c59cc279de2a4825f41323c7
damedeiros <davisson.medeiros@compsisnet.com.br>
2017-02-26 23:57:33 -0300

    remove injeção dos objetos para facilitar a criação nos endpoints

--- commons/commons-domain/src/main/java/com/dvsmedeiros/commons/domain/Result.java
+++ commons/commons-domain/src/main/java/com/dvsmedeiros/commons/domain/Result.java
@@ -1,18 +1,18 @@
 package com.dvsmedeiros.commons.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Result<T extends DomainEntity> extends ApplicationEntity {
 	
-	@Autowired
 	private List<T> entityList;
 	private T entity;
 	private String message;
 	private boolean error;
+	
+	public Result() {
+		this.entityList = new ArrayList<>();
+	}
 	
 	public String getMessage() {
 		return message;
@@ -31,7 +31,7 @@ public class Result<T extends DomainEntity> extends ApplicationEntity {
 	}
 	
 	public boolean hasError(){
-		if(!error && message == null && entityList != null && entityList.size() > 0){
+		if(!error && message == null && entityList != null){
 			return false;
 		}
 		return true;
