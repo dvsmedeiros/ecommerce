0a936225419eefcc410dcd0ab6cf51fec710a3ca
damedeiros <davisson.medeiros@compsisnet.com.br>
2017-03-13 10:26:56 -0300

    unifica metodos referentes a DomainSpecificEntity

--- commons/commons-core/src/main/java/com/dvsmedeiros/commons/controller/IFacade.java
+++ commons/commons-core/src/main/java/com/dvsmedeiros/commons/controller/IFacade.java
@@ -1,6 +1,7 @@
 package com.dvsmedeiros.commons.controller;
 
 import com.dvsmedeiros.commons.domain.DomainEntity;
+import com.dvsmedeiros.commons.domain.DomainSpecificEntity;
 import com.dvsmedeiros.commons.domain.Filter;
 import com.dvsmedeiros.commons.domain.IEntity;
 import com.dvsmedeiros.commons.domain.Result;
@@ -18,5 +19,11 @@ public interface IFacade<T extends DomainEntity> extends IEntity {
 	public Result<T> find(Long id, Class<? extends DomainEntity> clazz, INavigationCase<T> aCase);
 	
 	public Result<T> find(Filter<T> aFilter, INavigationCase<T> aCase);
+	
+	public Result<T> find(String code, Class<? extends DomainSpecificEntity> clazz, INavigationCase<T> aCase);
+	
+	public Result<T> delete(String code, Class<? extends DomainSpecificEntity> clazz, INavigationCase<T> aCase);
+	
+	public Result<T> findAll(boolean active, Class<? extends DomainSpecificEntity> clazz, INavigationCase<T> aCase);
 
 }
