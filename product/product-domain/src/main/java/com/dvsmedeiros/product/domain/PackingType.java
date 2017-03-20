c302efdc53378f3b32e5564f482ed52028e0c335
damedeiros <davisson.medeiros@compsisnet.com.br>
2017-03-20 16:41:18 -0300

    add tipo de pacote envelope

--- product/product-domain/src/main/java/com/dvsmedeiros/product/domain/PackingType.java
+++ product/product-domain/src/main/java/com/dvsmedeiros/product/domain/PackingType.java
@@ -1,7 +1,17 @@
 package com.dvsmedeiros.product.domain;
 
 public enum PackingType {
-	
-	BOX, ROLL;
-	
+
+	BOX(1), ROLL(2), ENVELOPE(3);
+
+	private int value;
+
+	private PackingType(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
 }
