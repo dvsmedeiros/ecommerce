47b1d2334ac7a34684dfbcaa28714c75d3b3c9df
Davisson <dvisson.medeiros@gmail.com>
2017-04-08 12:42:55 -0300

    Add componente de Fornecedor

---
+++ supplier/supplier-domain/src/main/java/com/dvsmedeiros/product/domain/Supplier.java
@@ -0,0 +1,14 @@
+package com.dvsmedeiros.product.domain;
+
+import javax.persistence.Entity;
+
+import org.springframework.stereotype.Component;
+
+import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
+
+@Component
+@Entity
+public class Supplier extends DomainSpecificEntity {
+	
+	
+}
