22643c966c3308de6306e810fbfd478e13f7d1dd
Davisson <dvisson.medeiros@gmail.com>
2017-04-22 00:47:34 -0300

    Corrige nome da classe de configuração do componente rest do comuns

--- commons/commons-core/src/main/java/com/dvsmedeiros/supplier/controller/business/CommonsNavigation.java
+++ commons/commons-core/src/main/java/com/dvsmedeiros/supplier/controller/business/CommonsNavigation.java
@@ -3,6 +3,6 @@ package com.dvsmedeiros.supplier.controller.business;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SupplierNavigation {
+public class CommonsNavigation {
 	
 }
