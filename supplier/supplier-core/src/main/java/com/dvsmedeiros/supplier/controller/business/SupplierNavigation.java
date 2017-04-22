129f73dea9512ff3f30663c0a782ea6058e0e9d0
Davisson <dvisson.medeiros@gmail.com>
2017-04-22 00:53:43 -0300

    Add navegacao para salvar um fornecedor

--- supplier/supplier-core/src/main/java/com/dvsmedeiros/supplier/controller/business/SupplierNavigation.java
+++ supplier/supplier-core/src/main/java/com/dvsmedeiros/supplier/controller/business/SupplierNavigation.java
@@ -1,8 +1,19 @@
 package com.dvsmedeiros.supplier.controller.business;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.dvsmedeiros.bce.controller.impl.EntityRuleDefinition;
+import com.dvsmedeiros.supplier.domain.Supplier;
 
 @Configuration
 public class SupplierNavigation {
-	
+
+	@Bean(name = "SAVE_SUPPLIER")
+	public EntityRuleDefinition<Supplier> getSaveProductNavigation() {
+
+		EntityRuleDefinition<Supplier> activities = new EntityRuleDefinition<>();
+		return activities;
+	}
+
 }
