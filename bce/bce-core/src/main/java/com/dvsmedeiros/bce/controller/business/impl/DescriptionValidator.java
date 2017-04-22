a6ad7fe7bb4a17d591721036b9dde87363ca62e4
Davisson <dvisson.medeiros@gmail.com>
2017-04-22 02:20:20 -0300

    Corrige validador de description que foi movido de DomainEntity para DomainSpecificEntity

--- bce/bce-core/src/main/java/com/dvsmedeiros/bce/controller/business/impl/DescriptionValidator.java
+++ bce/bce-core/src/main/java/com/dvsmedeiros/bce/controller/business/impl/DescriptionValidator.java
@@ -9,16 +9,16 @@ import com.dvsmedeiros.bce.domain.DomainEntity;
 import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 
 @Component
-public class DescriptionValidator implements IStrategy<DomainEntity>{
+public class DescriptionValidator implements IStrategy<DomainSpecificEntity>{
 
 	@Override
-	public void process(DomainEntity aEntity, INavigationCase<DomainEntity> aCase) {
+	public void process(DomainSpecificEntity aEntity, INavigationCase<DomainSpecificEntity> aCase) {
 		if(aEntity != null && aEntity.getDescription() != null && !StringUtils.isEmpty(aEntity.getDescription())){
 			return; 
 		}
 		
 		aCase.suspendExecution();
-		aCase.getResult().setMessage("Descri��o inexistente ou inv�lida!");
+		aCase.getResult().setMessage("Descrição inexistente ou inválida!");
 		
 	}
 
