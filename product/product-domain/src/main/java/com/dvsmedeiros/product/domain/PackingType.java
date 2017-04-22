19701e50a84edb1b3029ebc7e112fd6c203ea91a
Davisson <dvisson.medeiros@gmail.com>
2017-04-22 00:53:03 -0300

    Configura atributo para utilizar a especialização na criação da coluna

--- product/product-domain/src/main/java/com/dvsmedeiros/product/domain/PackingType.java
+++ product/product-domain/src/main/java/com/dvsmedeiros/product/domain/PackingType.java
@@ -1,5 +1,9 @@
 package com.dvsmedeiros.product.domain;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+
+@AttributeOverride(name = "value", column = @Column(name = "PACKING_TYPE"))
 public enum PackingType {
 
 	BOX(1), ROLL(2), ENVELOPE(3);
