8d2616c228068251e24e77994d5737726701058d
Davisson <dvisson.medeiros@gmail.com>
2017-04-28 00:42:17 -0300

    Add Validação para verificar se há um hacking valido no produto

---
+++ freight/freight-core/src/main/java/com/dvsmedeiros/freight/business/impl/PackingValidator.java
@@ -0,0 +1,23 @@
+package com.dvsmedeiros.freight.business.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.dvsmedeiros.bce.controller.INavigationCase;
+import com.dvsmedeiros.bce.controller.business.IStrategy;
+import com.dvsmedeiros.freight.domain.FreightFilter;
+
+@Component
+public class PackingValidator implements IStrategy<FreightFilter> {
+
+	@Override
+	public void process(FreightFilter aEntity, INavigationCase<FreightFilter> aCase) {
+		
+		if(aEntity != null && aEntity.getEntity() != null && aEntity.getEntity().getProduct() != null && aEntity.getEntity().getProduct().getPacking() != null){
+			return;
+		}
+		
+		aCase.suspendExecution();
+		aCase.getResult().setMessage("Pacote Inexistente ou inválido!");
+	}
+
+}
