b72d5a1f9e2deb677636f2f46940f09b2f8bb3f1
Davisson <dvisson.medeiros@gmail.com>
2017-04-28 00:43:28 -0300

    Add validação para verificar os valores mínimos de um pacote pelo seu tipo

---
+++ freight/freight-core/src/main/java/com/dvsmedeiros/freight/business/impl/MinimalValuesFreightValidator.java
@@ -0,0 +1,44 @@
+package com.dvsmedeiros.freight.business.impl;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.dvsmedeiros.bce.controller.INavigationCase;
+import com.dvsmedeiros.bce.controller.INavigator;
+import com.dvsmedeiros.bce.controller.business.IStrategy;
+import com.dvsmedeiros.bce.controller.impl.BusinessCase;
+import com.dvsmedeiros.bce.controller.impl.BusinessCaseBuilder;
+import com.dvsmedeiros.freight.domain.FreightFilter;
+import com.dvsmedeiros.product.domain.PackingType;
+
+@Component
+public class MinimalValuesFreightValidator implements IStrategy<FreightFilter> {
+
+	@Autowired
+	@Qualifier("navigator")
+	private INavigator navigator;
+
+	@Autowired
+	@Qualifier("MIN_VALUES_PACKING")
+	private Map<PackingType, String> minValues;
+
+	@Override
+	public void process(FreightFilter aEntity, INavigationCase<FreightFilter> aCase) {
+
+		String bCaseName = minValues.get(aEntity.getEntity().getProduct().getPacking().getType());
+
+		BusinessCase<FreightFilter> bCase = new BusinessCaseBuilder<FreightFilter>().withName(bCaseName).build();
+		navigator.run(aEntity, bCase);
+
+		if (bCase.isSuspendExecution()) {
+			aCase.suspendExecution();
+			aCase.getResult().setMessage(bCase.getResult().getMessage());
+			return;
+		}
+
+	}
+
+}
