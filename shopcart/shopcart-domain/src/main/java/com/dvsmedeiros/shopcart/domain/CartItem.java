ae2fcba35de2e124c80f0a6c786e91652f29f058
Davisson <dvisson.medeiros@gmail.com>
2017-04-29 21:56:16 -0300

    Refatora classe que descreve um item do carrinho

--- shopcart/shopcart-domain/src/main/java/com/dvsmedeiros/shopcart/domain/CartItem.java
+++ shopcart/shopcart-domain/src/main/java/com/dvsmedeiros/shopcart/domain/CartItem.java
@@ -1,23 +1,16 @@
 package com.dvsmedeiros.shopcart.domain;
 
-import com.dvsmedeiros.bce.domain.DomainEntity;
-import com.dvsmedeiros.product.domain.Price;
+import com.dvsmedeiros.commons.domain.Item;
 import com.dvsmedeiros.product.domain.Product;
 
-public class CartItem extends DomainEntity {
-
-	private static final int MINIMAL_QUANTITY = 1;
-
+public class CartItem extends Item {
+	
 	private Product product;
-	private int quantity;
-	private Price salePrice;
-
+	
 	public CartItem() {
 		this.product = new Product();
-		this.salePrice = new Price();
-		this.quantity = MINIMAL_QUANTITY;
 	}
-
+	
 	public void moreOneProduct() {
 		this.quantity++;
 	}
@@ -25,22 +18,13 @@ public class CartItem extends DomainEntity {
 	public void lessOneProduct() {
 		this.quantity--;
 	}
-	
+
 	public Product getProduct() {
 		return product;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public Price getSalePrice() {
-		return salePrice;
 	}
 
 	public void setProduct(Product product) {
 		this.product = product;
 	}
-	
 	
 }
