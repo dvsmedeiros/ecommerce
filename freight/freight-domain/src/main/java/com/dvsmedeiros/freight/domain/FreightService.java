f90243ef240230ba6e24c634e9064a9c36c7a659
Davisson <dvisson.medeiros@gmail.com>
2017-05-02 22:44:08 -0300

    Add endereço e mapeamento hibernate

--- freight/freight-domain/src/main/java/com/dvsmedeiros/freight/domain/FreightService.java
+++ freight/freight-domain/src/main/java/com/dvsmedeiros/freight/domain/FreightService.java
@@ -1,10 +1,13 @@
 package com.dvsmedeiros.freight.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
+import com.dvsmedeiros.address.domain.Address;
 import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 
 @Component
@@ -14,7 +17,9 @@ public class FreightService extends DomainSpecificEntity {
 
 	private Double value;
 	private String deadline;
-
+	@ManyToOne(cascade = CascadeType.DETACH)
+	private Address address;
+	
 	public Double getValue() {
 		return value;
 	}
@@ -31,4 +36,12 @@ public class FreightService extends DomainSpecificEntity {
 		this.deadline = deadline;
 	}
 
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
 }
