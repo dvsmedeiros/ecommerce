c9bb2cf3a1e302a55055b292cd2e9d4790325673
Davisson <dvisson.medeiros@gmail.com>
2017-05-02 22:41:00 -0300

    Refatora nome dos status

--- order/order-domain/src/main/java/com/dvsmedeiros/order/domain/StatusOrder.java
+++ order/order-domain/src/main/java/com/dvsmedeiros/order/domain/StatusOrder.java
@@ -2,7 +2,7 @@ package com.dvsmedeiros.order.domain;
 
 public enum StatusOrder {
 	
-	REALIZADO(1), PAGAMENTO(2), SEPARACAO(3), TRANSPORTE(4), ENTREGUE(5);
+	RECEBIDO(1), PAGAMENTO(2), SEPARACAO(3), TRANSPORTE(4), ENTREGUE(5), CANCELADO(6);
 
 	private int value;
 
