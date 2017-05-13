81775c06b54d6be8d17bc266e6785aaf6d0e46b8
Davisson <dvisson.medeiros@gmail.com>
2017-05-12 23:17:56 -0300

    Configura Spring Security

---
+++ ecommerce/src/main/java/com/dvsmedeiros/ecommerce/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,7 @@
+package com.dvsmedeiros.ecommerce.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer {
+
+}
