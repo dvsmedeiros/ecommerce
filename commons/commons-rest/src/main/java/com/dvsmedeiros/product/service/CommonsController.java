1c7b072b6db91367907825077cbdad763f51e751
Davisson <dvisson.medeiros@gmail.com>
2017-09-09 01:16:58 -0300

    refatora para compatibilidade com ice

--- commons/commons-rest/src/main/java/com/dvsmedeiros/product/service/CommonsController.java
+++ commons/commons-rest/src/main/java/com/dvsmedeiros/product/service/CommonsController.java
@@ -2,7 +2,7 @@ package com.dvsmedeiros.product.service;
 
 import org.springframework.stereotype.Controller;
 
-import com.dvsmedeiros.bce.service.BaseController;
+import com.dvsmedeiros.rest.rest.controller.BaseController;
 
 @Controller
 public class CommonsController extends BaseController {
