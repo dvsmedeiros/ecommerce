3c4d171ef4bf9b65c92df965e324204e5cf1462f
Davisson <dvisson.medeiros@gmail.com>
2017-09-09 00:29:38 -0300

    refatora para compatibilidade com bce

--- payment/payment-rest/src/main/java/com/dvsmedeiros/payment/service/PaymentController.java
+++ payment/payment-rest/src/main/java/com/dvsmedeiros/payment/service/PaymentController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
-import com.dvsmedeiros.bce.controller.IFacade;
-import com.dvsmedeiros.bce.controller.INavigator;
+import com.dvsmedeiros.bce.core.controller.IFacade;
+import com.dvsmedeiros.bce.core.controller.INavigator;
 import com.dvsmedeiros.payment.domain.Payment;
 
 @Controller
