e74c7893dd70e4fe8746921b09940c0f0d5fe31a
Davisson <dvisson.medeiros@gmail.com>
2017-09-27 09:05:01 -0300

    add scheduler para execução de tasks

--- ecommerce/src/main/java/com/dvsmedeiros/ecommerce/api/EcommerceApiApplication.java
+++ ecommerce/src/main/java/com/dvsmedeiros/ecommerce/api/EcommerceApiApplication.java
@@ -3,18 +3,30 @@ package com.dvsmedeiros.ecommerce.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-import com.dvsmedeiros.bce.core.BceCoreApplication;
-
-@Import(BceCoreApplication.class)
 @ComponentScan("com.dvsmedeiros")
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
 public class EcommerceApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApiApplication.class, args);
 	}
+	
+	@Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler
+          = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix(
+          "ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
 }
