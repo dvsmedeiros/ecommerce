7024977e29337f63cf61c6abfd6af1383739c504
Davisson <dvisson.medeiros@gmail.com>
2017-11-03 19:12:40 -0200

    refatora para herança para DomainSpecificEntityController<Author>

--- product/product-rest/src/main/java/com/dvsmedeiros/product/service/AuthorController.java
+++ product/product-rest/src/main/java/com/dvsmedeiros/product/service/AuthorController.java
@@ -1,8 +1,5 @@
 package com.dvsmedeiros.product.service;
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,41 +8,9 @@ import com.dvsmedeiros.rest.rest.controller.DomainSpecificEntityController;
 
 @Controller
 @RequestMapping("${bce.app.context}/author")
-@SuppressWarnings("rawtypes")
 public class AuthorController extends DomainSpecificEntityController<Author>{
 
 	public AuthorController() {
 		super(Author.class);
 	}
-	/*
-	@Override
-	@Cacheable(value = "cacheAuthors")
-	public ResponseEntity getEntities() {
-		return super.getEntities();
-	}
-	
-	@Override
-	@CacheEvict(value = "cacheAuthors", allEntries = true)
-	public ResponseEntity createEntity(Author entity) {
-		return super.createEntity(entity);
-	}
-	
-	@Override
-	@CacheEvict(value = "cacheAuthors", allEntries = true)
-	public ResponseEntity updateEntity(Author entity) {
-		return super.updateEntity(entity);
-	}
-	
-	@Override
-	@CacheEvict(value = "cacheAuthors", allEntries = true)
-	public ResponseEntity deleteEntityById(Long id) {
-		return super.deleteEntityById(id);
-	}
-	
-	@Override
-	@CacheEvict(value = "cacheAuthors", allEntries = true)
-	public ResponseEntity inactivateEntityById(Long id) {
-		return super.inactivateEntityById(id);
-	}
-	*/
 }
