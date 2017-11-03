5ecfe9a7ce4005760c4b0b6fc33bee6bb0711d0a
Davisson <dvisson.medeiros@gmail.com>
2017-11-03 19:08:48 -0200

    remove confirações da categoria

--- category/category-domain/src/main/java/com/dvsmedeiros/category/domain/Category.java
+++ category/category-domain/src/main/java/com/dvsmedeiros/category/domain/Category.java
@@ -1,22 +1,13 @@
 package com.dvsmedeiros.category.domain;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 import org.springframework.stereotype.Component;
 
-import com.dvsmedeiros.bce.domain.Configuration;
 import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 
 @Component
@@ -25,23 +16,12 @@ import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Category extends DomainSpecificEntity {
 
-	@OneToMany(cascade = CascadeType.MERGE)
-	@LazyCollection(LazyCollectionOption.FALSE)
-	private List<Configuration> configs;	
 	@ManyToOne
 	private CategoryType type;
 	
 	public Category() {
 	}
 	
-	public List<Configuration> getConfigs() {
-		return configs;
-	}
-
-	public void setConfigs(List<Configuration> configs) {
-		this.configs = configs;
-	}
-
 	public CategoryType getType() {
 		return type;
 	}
