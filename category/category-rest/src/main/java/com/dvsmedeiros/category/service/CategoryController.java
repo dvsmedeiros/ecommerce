1a8f7ec3d44d47eddf9d53347579a26076ec8b8c
Davisson <dvisson.medeiros@gmail.com>
2017-11-03 23:18:41 -0200

    remove comentários

--- category/category-rest/src/main/java/com/dvsmedeiros/category/service/CategoryController.java
+++ category/category-rest/src/main/java/com/dvsmedeiros/category/service/CategoryController.java
@@ -1,8 +1,5 @@
 package com.dvsmedeiros.category.service;
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,42 +8,10 @@ import com.dvsmedeiros.rest.rest.controller.DomainSpecificEntityController;
 
 @Controller
 @RequestMapping("${bce.app.context}/category")
-@SuppressWarnings("rawtypes")
 public class CategoryController extends DomainSpecificEntityController<Category>{
 
 	public CategoryController() {
 		super(Category.class);
 	}
-	/*
-	@Override
-	@Cacheable(value = "cacheCategories")
-	public ResponseEntity getEntities() {
-		return super.getEntities();
-	}
 	
-	@Override
-	@CacheEvict(value = "cacheCategories", allEntries = true)
-	public ResponseEntity createEntity(Category entity) {
-		// TODO Auto-generated method stub
-		return super.createEntity(entity);
-	}
-
-	@Override
-	@CacheEvict(value = "cacheCategories", allEntries = true)
-	public ResponseEntity updateEntity(Category entity) {
-		return super.updateEntity(entity);
-	}
-	
-	@Override
-	@CacheEvict(value = "cacheCategories", allEntries = true)
-	public ResponseEntity deleteEntityById(Long id) {
-		return super.deleteEntityById(id);
-	}
-
-	@Override
-	@CacheEvict(value = "cacheCategories", allEntries = true)
-	public ResponseEntity inactivateEntityById(Long id) {
-		return super.inactivateEntityById(id);
-	}
-	*/
 }
--- product/product-rest/src/main/java/com/dvsmedeiros/product/service/PublisherController.java
+++ product/product-rest/src/main/java/com/dvsmedeiros/product/service/PublisherController.java
@@ -1,8 +1,5 @@
 package com.dvsmedeiros.product.service;
 
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,41 +8,10 @@ import com.dvsmedeiros.rest.rest.controller.DomainSpecificEntityController;
 
 @Controller
 @RequestMapping("${bce.app.context}/publisher")
-@SuppressWarnings("rawtypes")
 public class PublisherController extends DomainSpecificEntityController<Publisher>{
 
 	public PublisherController() {
 		super(Publisher.class);
 	}
 	
-	/*
-	@Override
-	@Cacheable(value = "cachePublishers")
-	public ResponseEntity getEntities() {
-		return super.getEntities();
-	}
-	
-	@Override
-	@CacheEvict(value = "cachePublishers", allEntries = true)
-	public ResponseEntity createEntity(Publisher entity) {
-		return super.createEntity(entity);
-	}
-	
-	@Override
-	@CacheEvict(value = "cachePublishers", allEntries = true)
-	public ResponseEntity updateEntity(Publisher entity) {
-		return super.updateEntity(entity);
-	}
-	
-	@CacheEvict(value = "cachePublishers", allEntries = true)
-	public ResponseEntity deleteEntityById(Long id) {
-		return super.deleteEntityById(id);
-	}
-	
-	@Override
-	@CacheEvict(value = "cachePublishers", allEntries = true)
-	public ResponseEntity inactivateEntityById(Long id) {
-		return super.inactivateEntityById(id);
-	}
-	*/
 }
